8ccd39d5032ca53c0b6d983949cb74cda059027b
the-dagger <harshithdwivedi@gmail.com>
2018-09-09 18:01:08 +0530

    refactor : use androidx
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

--- app/src/main/java/com/codingblocks/cbonlineapp/MainActivity.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.codingblocks.cbonlineapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
