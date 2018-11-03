59cd0d5004c8dd5a76c20e0d62ee4286d7432144
pulkit-mac <aggarwalpulkit596@gmail.com>
2018-11-03 15:33:16 +0530

    Courses Activity Complete
    Signed-off-by: pulkit-mac <aggarwalpulkit596@gmail.com>

--- app/src/main/java/com/codingblocks/cbonlineapp/adapters/SectionsDataAdapter.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/adapters/SectionsDataAdapter.kt
@@ -4,11 +4,15 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.codingblocks.cbonlineapp.R
 import com.codingblocks.onlineapi.models.Sections
+import kotlinx.android.synthetic.main.item_section.view.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import java.util.*
@@ -46,66 +50,75 @@ class SectionsDataAdapter(private var sectionData: ArrayList<Sections>?) : Recyc
 
         fun bindView(data: Sections) {
 
-//            itemView.sections.title.text = data.name
-//            itemView.sections.setDescription1("${data.contents?.size} Lectures")
-//            var duration: Long = 0
-//            for (subitems in data.contents!!) {
-//                duration += subitems.duration!!
-//            }
-//            val hour = duration / (1000 * 60 * 60) % 24
-//            val minute = duration / (1000 * 60) % 60
-//            if (hour <= 0)
-//                itemView.sections.setDescription2("$minute Mins")
-//            else if (minute <= 0)
-//                itemView.sections.setDescription2("$hour Hours")
-//            else
-//                itemView.sections.setDescription2("---")
-//            val myView = factory.inflate(R.layout.item_section_content_info, null)
-//            myView.textView15.text = "Pulkit"
-//            val params = LinearLayout.LayoutParams(
-//                    FrameLayout.LayoutParams.MATCH_PARENT,
-//                    FrameLayout.LayoutParams.WRAP_CONTENT)
-//
-//            val myRoot = itemView.findViewById<LinearLayout>(R.id.sectionRoot)
-//            val a = LinearLayout(context)
-//            a.orientation = LinearLayout.HORIZONTAL
-//
-//            val card: ExpandableCardView = itemView.findViewById(R.id.sections)
-//            for (i in data.contents!!) {
-//                myView.textView15.text = "Pulkit"
-//                a.addView(myView)
-//            }
-//            myRoot.addView(a)
+            itemView.title.text = data.name
+            itemView.lectures.text = ("${data.contents?.size} Lectures")
+            var duration: Long = 0
+            for (subitems in data.contents!!) {
+                if (subitems.contentable == "lecture" || subitems.contentable == "video")
+                    duration += subitems.duration!!
+            }
+            val hour = duration / (1000 * 60 * 60) % 24
+            val minute = duration / (1000 * 60) % 60
+            info { "hour$hour   minute$minute" }
 
-            val ll = itemView.findViewById<LinearLayout>(R.id.sectionRoot)
+            if (minute >= 1 && hour == 0L)
+                itemView.lectureTime.text = ("$minute Min")
+            else if (hour >= 1) {
+                itemView.lectureTime.text = ("$hour Hours")
+            } else
+                itemView.lectureTime.text = ("---")
+
+            val ll = itemView.findViewById<LinearLayout>(R.id.sectionContents)
             ll.orientation = LinearLayout.VERTICAL
+            ll.visibility = View.GONE
 
             for (i in data.contents!!) {
                 val factory = LayoutInflater.from(context)
                 val inflatedView = factory.inflate(R.layout.item_section_content_info, ll, false)
-                val submittedByTextView = inflatedView.findViewById(R.id.textView15) as TextView
-                submittedByTextView.text = i.title
+                val subTitle = inflatedView.findViewById(R.id.textView15) as TextView
+                val subDuration = inflatedView.findViewById(R.id.textView16) as TextView
+                val contentImg = inflatedView.findViewById(R.id.imageView3) as ImageView
+                if (i.contentable == "lecture" || i.contentable == "video") {
+                    val contentDuration: Long = i.duration!!
+                    contentImg.setImageDrawable(context.getDrawable(R.drawable.video_green_dark))
+                    val contenthour = contentDuration / (1000 * 60 * 60) % 24
+                    val contentminute = contentDuration / (1000 * 60) % 60
+                    when {
+                        contenthour <= 0 -> subDuration.text = ("$contentminute Mins")
+                        contentminute <= 0 -> subDuration.text = ("$contenthour Hours")
+                        else -> itemView.lectureTime.text = ("---")
+                    }
+                } else if (i.contentable == "document") {
+                    contentImg.setImageDrawable(context.getDrawable(R.drawable.file_green_dark))
+
+                } else if (i.contentable == "code-challenge") {
+                    contentImg.setImageDrawable(context.getDrawable(R.drawable.code_green_dark))
+                }
+                subTitle.text = i.title
+
+
                 ll.addView(inflatedView)
+            }
+            var arrowAnimation: RotateAnimation
+
+            itemView.arrow.setOnClickListener {
+                if (ll.visibility == View.GONE) {
+                    ll.visibility = View.VISIBLE
+                    arrowAnimation = RotateAnimation(0f, 180f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                            0.5f)
+                    arrowAnimation.fillAfter = true
+                    arrowAnimation.duration = 350
+                    itemView.arrow.startAnimation(arrowAnimation)
+                } else {
+                    ll.visibility = View.GONE
+                    arrowAnimation = RotateAnimation(180f, 0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                            0.5f)
+                    arrowAnimation.fillAfter = true
+                    arrowAnimation.duration = 350
+                    itemView.arrow.startAnimation(arrowAnimation)
+                }
 
             }
-
-//            itemView.sections.setOnExpandedListener { v, isExpanded ->
-//                if (isExpanded) {
-//                    val view = v.findViewById<LinearLayout>(R.id.rootView)
-//                    info {
-//                        "view" +
-//                                view.toString()
-//                    }
-//
-//                }
-//            }
-//            for ( i in data.contents!!){
-//                myView.textView15.text = i.title
-//                val params = RelativeLayout.LayoutParams(
-//                        FrameLayout.LayoutParams.MATCH_PARENT,
-//                        FrameLayout.LayoutParams.WRAP_CONTENT)
-//                itemView.sections.addView(myView, params)
-//            }
 
 
         }
