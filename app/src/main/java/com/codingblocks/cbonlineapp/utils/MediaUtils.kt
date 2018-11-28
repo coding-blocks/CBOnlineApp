f4a0ca6900d77be30bfc486836a2b8f50b96ee3a
the-dagger <harshithdwivedi@gmail.com>
2018-11-28 17:38:36 +0530

    m3u8 file read
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

--- app/src/main/java/com/codingblocks/cbonlineapp/utils/MediaUtils.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/utils/MediaUtils.kt
@@ -7,6 +7,7 @@ import java.io.File
 
 object MediaUtils {
 
+    //Call this when you want to play a video and pass the return type to exoplayer
     fun getCourseVideoUri(videoUrl: String, context: Context): Uri {
         //TODO : Request storage permissions on the app launch
         val file = context.getExternalFilesDir(Environment.getDataDirectory().absolutePath)
@@ -14,9 +15,15 @@ object MediaUtils {
         return Uri.parse(dataFile.toURI().toString())
     }
 
-    fun getCourseDownloadUrls(m3u8Uri: String, context: Context): ArrayList<String> {
-        //TODO
-        return arrayListOf()
+    fun getCourseDownloadUrls(videoUrl: String, context: Context): ArrayList<String> {
+        val videoNames = arrayListOf<String>()
+
+        val file = context.getExternalFilesDir(Environment.getDataDirectory().absolutePath)
+        val dataFile = File(file, "/$videoUrl/video.m3u8")
+
+        //Read the file above and add the ts names to videoNames
+
+        return videoNames
     }
 
 }
