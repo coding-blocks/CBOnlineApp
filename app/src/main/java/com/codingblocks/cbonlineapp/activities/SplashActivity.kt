6d56d83dae01f39ef5a67d36e15d0a7b7c28935b
Arnav Gupta <arnav@codingblocks.com>
2018-12-19 07:44:33 +0530

    fix splashscreen using coroutine, improve styles
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- app/src/main/java/com/codingblocks/cbonlineapp/activities/SplashActivity.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/activities/SplashActivity.kt
@@ -5,42 +5,38 @@ import android.os.Handler
 import android.view.Gravity
 import android.view.View
 import android.widget.ImageView
+import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityOptionsCompat
 import com.codingblocks.cbonlineapp.R
 import com.codingblocks.cbonlineapp.prefs
+import kotlinx.coroutines.*
 import org.jetbrains.anko.*
+import org.jetbrains.anko.coroutines.experimental.asReference
+import org.jetbrains.anko.coroutines.experimental.bg
+import org.jetbrains.anko.custom.async
 
 
 class SplashActivity : AppCompatActivity() {
-    val ui = SplashActivityUI()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ui.setContentView(this)
-        scheduleSplashScreen()
+        GlobalScope.launch {
+            finishSplashScreen()
+        }
     }
 
-    private fun scheduleSplashScreen() {
-        val splashScreenDuration = getSplashScreenDuration()
-        Handler().postDelayed(
-                {
-                    // After the splash screen duration, route to the right activities
-                    if (prefs.SP_ACCESS_TOKEN_KEY == "access_token") {
-                        val compat = ActivityOptionsCompat.makeSceneTransitionAnimation(this, ui.logo, "trans1")
-                        startActivity(intentFor<LoginActivity>().singleTop(), compat.toBundle())
-                        finish()
-                    } else {
-                        startActivity(intentFor<HomeActivity>().singleTop())
-                        finish()
-                    }
-                },
-                splashScreenDuration
-        )
+    suspend fun getAccessToken() = prefs.SP_ACCESS_TOKEN_KEY
+
+    suspend fun finishSplashScreen() {
+        // After the splash screen duration, route to the right activities
+        if (getAccessToken() == "access_token") {
+            startActivity(intentFor<LoginActivity>().singleTop())
+            finish()
+        } else {
+            startActivity(intentFor<HomeActivity>().singleTop())
+            finish()
+        }
     }
-
-
-    private fun getSplashScreenDuration() = 3000L
 
     class SplashActivityUI : AnkoComponent<SplashActivity> {
         lateinit var logo: ImageView
