68eb00697912b61ca8f972d4c00c1bcafa21db6e
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-01-15 23:57:20 +0530

    Leaderboard added

---
+++ app/src/main/java/com/codingblocks/cbonlineapp/adapters/LeaderboardListAdapter.kt
@@ -0,0 +1,54 @@
+package com.codingblocks.cbonlineapp.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.codingblocks.cbonlineapp.R
+import com.codingblocks.onlineapi.models.Leaderboard
+import org.jetbrains.anko.AnkoLogger
+import java.util.*
+
+class LeaderboardListAdapter(internal var context: Context,
+                             var list: ArrayList<Leaderboard>?
+) : ArrayAdapter<Leaderboard>(context, 0), AnkoLogger {
+
+    override fun getCount(): Int {
+        return if (list == null) 0 else list!!.size
+    }
+
+    override fun getItem(i: Int): Leaderboard? {
+        return list!![i]
+    }
+
+    override fun getItemId(i: Int): Long {
+        return i.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var view = convertView
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.quiz_attempt_list, parent, false)
+
+            val pos = view!!.findViewById<TextView>(R.id.numberTv)
+            val name = view.findViewById<TextView>(R.id.statusTv)
+            val college = view.findViewById<TextView>(R.id.dateTv)
+            val score = view.findViewById<TextView>(R.id.scoreTv)
+
+            val attemptViewHolder = ViewHolder(pos, name, college, score)
+            view.tag = attemptViewHolder
+        }
+        val e = getItem(position)//to differentiate between filtered list and todo list
+        val attemptViewHolder = view.tag as ViewHolder
+        attemptViewHolder.posTextView.text = (position + 1).toString() + ""
+        attemptViewHolder.nameTextView.text = e?.userName!!
+        attemptViewHolder.collegeTextView.text = e.collegeName
+        attemptViewHolder.scoreTextView.text = e.score.toString() ?:"N/a"
+
+        return view
+    }
+
+    class ViewHolder(var posTextView: TextView, var nameTextView: TextView, var collegeTextView: TextView, var scoreTextView: TextView)
+}
--- app/src/main/java/com/codingblocks/cbonlineapp/fragments/OverviewFragment.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/fragments/OverviewFragment.kt
@@ -8,9 +8,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.codingblocks.cbonlineapp.R
 import com.codingblocks.cbonlineapp.Utils.retrofitCallback
+import com.codingblocks.cbonlineapp.adapters.LeaderboardListAdapter
 import com.codingblocks.onlineapi.Clients
+import com.codingblocks.onlineapi.models.Leaderboard
+import kotlinx.android.synthetic.main.fragment_about_quiz.*
+import kotlinx.android.synthetic.main.fragment_overview.*
 import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.info
 
 private const val ARG__ATTEMPT_ID = "attempt_id"
 private const val ARG__RUN_ID = "run_id"
@@ -21,31 +24,32 @@ class OverviewFragment : Fragment(), AnkoLogger {
 
     lateinit var attemptId: String
     lateinit var runId: String
+    private lateinit var leaderboardListAdapter: LeaderboardListAdapter
+    private var list = ArrayList<Leaderboard>()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?):
+            View? = inflater.inflate(R.layout.fragment_overview, container, false).apply {
+        leaderboardListAdapter = LeaderboardListAdapter(context, list)
+    }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         arguments?.let {
             attemptId = it.getString(ARG__ATTEMPT_ID)!!
             runId = it.getString(ARG__RUN_ID)!!
 
         }
 
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?):
-            View? = inflater.inflate(R.layout.fragment_overview, container, false).apply {
-
-
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        val header = layoutInflater.inflate(R.layout.leaderboard_header, quizAttemptLv, false) as ViewGroup
+        leaderboardLv.addHeaderView(header, null, false)
+        leaderboardLv.adapter = leaderboardListAdapter
 
         Clients.api.leaderboardById(runId).enqueue(retrofitCallback { throwable, response ->
-
-            info { response?.body()?.size }
+            response?.body().let {
+                list.addAll(it as ArrayList<Leaderboard>)
+                leaderboardListAdapter.notifyDataSetChanged()
+            }
         })
     }
 
