9a32a2942f9e0823a87ef9a7ec6b1d0593d6c574
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-02-02 22:28:13 +0530

    post doubt from custom dialog

--- app/src/main/java/com/codingblocks/cbonlineapp/fragments/VideoDoubtFragment.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/fragments/VideoDoubtFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.codingblocks.cbonlineapp.R
+import kotlinx.android.synthetic.main.doubt_dialog.view.*
+import kotlinx.android.synthetic.main.fragment_video_doubt.view.*
+import org.jetbrains.anko.layoutInflater
 
 
 private const val ARG_PARAM1 = "param1"
@@ -26,8 +30,40 @@ class VideoDoubtFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_video_doubt, container, false)
+        val view = inflater.inflate(R.layout.fragment_video_doubt, container, false)
+
+        view.doubtFab.setOnClickListener {
+            showDialog()
+        }
+
+        return view
     }
+
+    private fun showDialog() {
+        val doubtDialog = AlertDialog.Builder(context!!).create()
+        val doubtView = context!!.layoutInflater.inflate(R.layout.doubt_dialog, null)
+        doubtView.cancelBtn.setOnClickListener {
+            doubtDialog.dismiss()
+        }
+        doubtView.okBtn.setOnClickListener {
+                if (doubtView.titleLayout.editText!!.text.length < 15 || doubtView.titleLayout.editText!!.text.isEmpty()) {
+                    doubtView.titleLayout.error = "Title length must be atleast 15 characters."
+                    return@setOnClickListener
+                }else if(doubtView.descriptionLayout.editText!!.text.length < 20 || doubtView.descriptionLayout.editText!!.text.isEmpty()) {
+                    doubtView.descriptionLayout.error = "Description length must be atleast 20 characters."
+                    doubtView.titleLayout.error = ""
+                }else{
+                    doubtView.descriptionLayout.error = ""
+
+                }
+            }
+
+        doubtDialog.window.setBackgroundDrawableResource(android.R.color.transparent)
+        doubtDialog.setView(doubtView)
+        doubtDialog.setCancelable(false)
+        doubtDialog.show()
+    }
+
 
     companion object {
         @JvmStatic
