07a3f9cf1f8a525a8e6b642bae66ec3965c242cd
Arnav Gupta <arnav@codingblocks.com>
2019-03-19 23:57:04 +0530

    disable crashlytics at runtime instead
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- app/src/main/java/com/codingblocks/cbonlineapp/CBOnlineApp.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/CBOnlineApp.kt
@@ -3,11 +3,14 @@ package com.codingblocks.cbonlineapp
 import android.app.Application
 import android.content.Context
 import com.codingblocks.onlineapi.CustomResponseInterceptor
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
 import com.devbrackets.android.exomedia.ExoMedia
 import com.google.android.exoplayer2.ext.okhttp.OkHttpDataSourceFactory
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.TransferListener
 import com.squareup.picasso.Picasso
+import io.fabric.sdk.android.Fabric
 import io.github.inflationx.calligraphy3.CalligraphyConfig
 import io.github.inflationx.calligraphy3.CalligraphyInterceptor
 import io.github.inflationx.viewpump.ViewPump
@@ -19,16 +22,26 @@ class CBOnlineApp : Application() {
         super.onCreate()
         mInstance = this
         Picasso.setSingletonInstance(Picasso.Builder(this).build())
-        ViewPump.init(ViewPump.builder()
-                .addInterceptor(CalligraphyInterceptor(
+        ViewPump.init(
+            ViewPump.builder()
+                .addInterceptor(
+                    CalligraphyInterceptor(
                         CalligraphyConfig.Builder()
-                                .setDefaultFontPath("fonts/Cabin-Medium.ttf")
-                                .setFontAttrId(R.attr.fontPath)
-                                .build()))
-                .build())
+                            .setDefaultFontPath("fonts/Cabin-Medium.ttf")
+                            .setFontAttrId(R.attr.fontPath)
+                            .build()
+                    )
+                )
+                .build()
+        )
         configureExoMedia()
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1)
 //            shortcutAction(::updateShortcuts)
+
+        val crashlyticsKit = Crashlytics.Builder()
+            .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+            .build()
+        Fabric.with(this, crashlyticsKit)
     }
 
     companion object {
@@ -44,7 +57,10 @@ class CBOnlineApp : Application() {
         ExoMedia.setDataSourceFactoryProvider(object : ExoMedia.DataSourceFactoryProvider {
             private var instance: DataSource.Factory? = null
 
-            override fun provide(userAgent: String, listener: TransferListener?): DataSource.Factory {
+            override fun provide(
+                userAgent: String,
+                listener: TransferListener?
+            ): DataSource.Factory {
                 if (instance == null) {
                     // Updates the network data source to use the OKHttp implementation
                     val interceptor = CustomResponseInterceptor()
