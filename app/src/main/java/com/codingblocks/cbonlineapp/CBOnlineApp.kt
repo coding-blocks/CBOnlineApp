113c84ffb44ae2c4f3f92dffb2bca9d32127e52d
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-04-09 22:58:39 +0530

    remove ExoMedia

--- app/src/main/java/com/codingblocks/cbonlineapp/CBOnlineApp.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/CBOnlineApp.kt
@@ -2,19 +2,13 @@ package com.codingblocks.cbonlineapp
 
 import android.app.Application
 import android.content.Context
-import com.codingblocks.onlineapi.CustomResponseInterceptor
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.core.CrashlyticsCore
-import com.devbrackets.android.exomedia.ExoMedia
-import com.google.android.exoplayer2.ext.okhttp.OkHttpDataSourceFactory
-import com.google.android.exoplayer2.upstream.DataSource
-import com.google.android.exoplayer2.upstream.TransferListener
 import com.squareup.picasso.Picasso
 import io.fabric.sdk.android.Fabric
 import io.github.inflationx.calligraphy3.CalligraphyConfig
 import io.github.inflationx.calligraphy3.CalligraphyInterceptor
 import io.github.inflationx.viewpump.ViewPump
-import okhttp3.OkHttpClient
 
 class CBOnlineApp : Application() {
 
@@ -34,9 +28,7 @@ class CBOnlineApp : Application() {
                 )
                 .build()
         )
-        configureExoMedia()
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1)
-//            shortcutAction(::updateShortcuts)
+
 
         val crashlyticsKit = Crashlytics.Builder()
             .core(CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
@@ -51,30 +43,4 @@ class CBOnlineApp : Application() {
         }
     }
 
-    private fun configureExoMedia() {
-        // Registers the media sources to use the OkHttp client instead of the standard Apache one
-        // Note: the OkHttpDataSourceFactory can be found in the ExoPlayer extension library `extension-okhttp`
-        ExoMedia.setDataSourceFactoryProvider(object : ExoMedia.DataSourceFactoryProvider {
-            private var instance: DataSource.Factory? = null
-
-            override fun provide(
-                userAgent: String,
-                listener: TransferListener?
-            ): DataSource.Factory {
-                if (instance == null) {
-                    // Updates the network data source to use the OKHttp implementation
-                    val interceptor = CustomResponseInterceptor()
-                    val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
-
-                    val upstreamFactory = OkHttpDataSourceFactory(client, userAgent, listener)
-                    instance = upstreamFactory
-                    // Adds a cache around the upstreamFactory
-//                    val cache = SimpleCache(File(cacheDir, "ExoMediaCache"), LeastRecentlyUsedCacheEvictor((50 * 1024 * 1024).toLong()))
-//                    instance = CacheDataSourceFactory(cache, upstreamFactory, CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR)
-                }
-
-                return instance!!
-            }
-        })
-    }
 }
