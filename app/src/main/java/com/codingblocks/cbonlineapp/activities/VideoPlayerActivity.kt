f0621214113be174d78a63a27fc2784f21f2d22a
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-04-09 22:58:39 +0530

    remove ExoMedia

--- app/src/main/java/com/codingblocks/cbonlineapp/activities/VideoPlayerActivity.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/activities/VideoPlayerActivity.kt
@@ -3,7 +3,6 @@ package com.codingblocks.cbonlineapp.activities
 import android.animation.LayoutTransition
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AlertDialog
@@ -18,8 +17,6 @@ import com.codingblocks.cbonlineapp.database.DoubtsModel
 import com.codingblocks.cbonlineapp.database.NotesModel
 import com.codingblocks.cbonlineapp.fragments.VideoDoubtFragment
 import com.codingblocks.cbonlineapp.fragments.VideoNotesFragment
-import com.codingblocks.cbonlineapp.utils.MediaUtils
-import com.codingblocks.cbonlineapp.utils.MyVideoControls
 import com.codingblocks.cbonlineapp.utils.OnItemClickListener
 import com.codingblocks.cbonlineapp.utils.pageChangeCallback
 import com.codingblocks.onlineapi.Clients
@@ -27,22 +24,19 @@ import com.codingblocks.onlineapi.models.Contents
 import com.codingblocks.onlineapi.models.DoubtsJsonApi
 import com.codingblocks.onlineapi.models.Notes
 import com.codingblocks.onlineapi.models.RunAttemptsModel
-import com.devbrackets.android.exomedia.listener.OnPreparedListener
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
 import com.google.android.youtube.player.YouTubePlayerSupportFragment
 import io.github.inflationx.viewpump.ViewPumpContextWrapper
 import kotlinx.android.synthetic.main.activity_video_player.*
 import kotlinx.android.synthetic.main.doubt_dialog.view.*
-import kotlinx.android.synthetic.main.exomedia_default_controls_mobile.view.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import kotlin.concurrent.thread
 
 
 class VideoPlayerActivity : AppCompatActivity(),
-        OnPreparedListener,
-        OnItemClickListener, AnkoLogger {
+    OnItemClickListener, AnkoLogger {
 
     private var youtubePlayer: YouTubePlayer? = null
     private var pos: Long? = 0
@@ -74,28 +68,18 @@ class VideoPlayerActivity : AppCompatActivity(),
         if (contentId == id) {
             if (displayYoutubeVideo.view?.visibility == View.VISIBLE)
                 youtubePlayer?.seekToMillis(position * 1000)
-            else
-                videoView.seekTo(position.toLong() * 1000)
-        }
 
+//                videoView.seekTo(position.toLong() * 1000)
+
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.codingblocks.cbonlineapp.R.layout.activity_video_player)
         rootLayout.layoutTransition
-                .enableTransitionType(LayoutTransition.CHANGING)
-        val controls = MyVideoControls(this)
-        videoView.setControls(controls)
-        (videoView.videoControls as MyVideoControls).let {
-            it.fullscreenBtn.setOnClickListener {
-                val i = Intent(this, VideoPlayerFullScreenActivity::class.java)
-                i.putExtra("FOLDER_NAME", videoView.videoUri.toString())
-                i.putExtra("CURRENT_POSITION", videoView.currentPosition)
-                startActivityForResult(i, 1)
-            }
+            .enableTransitionType(LayoutTransition.CHANGING)
 
-        }
 
         val url = intent.getStringExtra("FOLDER_NAME")
         val youtubeUrl = intent.getStringExtra("videoUrl")
@@ -133,11 +117,10 @@ class VideoPlayerActivity : AppCompatActivity(),
                 }
                 1 -> {
                     videoFab.setOnClickListener {
-                        val notePos: Double = if (displayYoutubeVideo.view?.visibility == View.VISIBLE)
-                            (youtubePlayer?.currentTimeMillis!! / 1000).toDouble()
-                        else
-                            (videoView.currentPosition.toInt() / 1000).toDouble()
-                        createNote(notePos)
+                        //                        val notePos: Double = if (displayYoutubeVideo.view?.visibility == View.VISIBLE)
+//                            (youtubePlayer?.currentTimeMillis!! / 1000).toDouble()
+////                            (videoView.currentPosition.toInt() / 1000).toDouble()
+//                        createNote(notePos=0.0)
                     }
                 }
             }
@@ -177,10 +160,10 @@ class VideoPlayerActivity : AppCompatActivity(),
                         if (response?.isSuccessful!!)
                             try {
                                 notesDao.insert(NotesModel(it!!.id
-                                        ?: "", it.duration ?: 0.0, it.text ?: "", it.content?.id
-                                        ?: "", attemptId, it.createdAt
-                                        ?: "", it.deletedAt
-                                        ?: ""))
+                                    ?: "", it.duration ?: 0.0, it.text ?: "", it.content?.id
+                                    ?: "", attemptId, it.createdAt
+                                    ?: "", it.deletedAt
+                                    ?: ""))
                             } catch (e: Exception) {
                                 info { "error" + e.localizedMessage }
                             }
@@ -214,36 +197,23 @@ class VideoPlayerActivity : AppCompatActivity(),
     }
 
     private fun setupVideoView(url: String, downloaded: Boolean) {
-        videoView.setOnPreparedListener(this)
-        if (downloaded) {
-            videoView.setVideoURI(MediaUtils.getCourseVideoUri(url, this))
-        } else {
-            videoView.setVideoURI(Uri.parse(url))
-        }
-        videoView.setOnCompletionListener {
-            finish()
-        }
+
     }
 
-    override fun onPrepared() {
-        videoView.start()
-    }
 
     override fun onPause() {
         super.onPause()
-        videoView.pause()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        videoView.release()
     }
 
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (resultCode == -1) {
             pos = data?.getLongExtra("CURRENT_POSITION", 0)
-            videoView.seekTo(pos ?: 0)
+//            videoView.seekTo(pos ?: 0)
         }
     }
 
@@ -285,8 +255,8 @@ class VideoPlayerActivity : AppCompatActivity(),
                             doubtDialog.dismiss()
                             thread {
                                 doubtsDao.insert(DoubtsModel(it!!.id
-                                        ?: "", it.title, it.body, it.content?.id
-                                        ?: "", it.status, it.runAttempt?.id ?: ""
+                                    ?: "", it.title, it.body, it.content?.id
+                                    ?: "", it.status, it.runAttempt?.id ?: ""
                                 ))
                             }
                         }
