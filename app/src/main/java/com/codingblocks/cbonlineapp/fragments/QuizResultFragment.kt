70c2a699418567404763d0bc0018068d3b2d6c8a
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2019-05-15 04:48:59 +0530

    created layout for quiz result screen

---
+++ app/src/main/java/com/codingblocks/cbonlineapp/fragments/QuizResultFragment.kt
@@ -0,0 +1,17 @@
+package com.codingblocks.cbonlineapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.codingblocks.cbonlineapp.R
+
+class QuizResultFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):
+        View? = inflater.inflate(R.layout.fragment_quiz_result, container, false).apply {
+
+    }
+
+}
