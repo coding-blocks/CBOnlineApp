de638e41a938e5e7f897e384abf4a6a6a3574a86
Prerak Mann <mannprerak2@gmail.com>
2019-05-15 21:37:12 +0530

    Fixed AboutActivity issues

--- app/src/main/java/com/codingblocks/cbonlineapp/activities/AboutActivity.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/activities/AboutActivity.kt
@@ -1,11 +1,13 @@
 package com.codingblocks.cbonlineapp.activities
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.codingblocks.cbonlineapp.R
 import kotlinx.android.synthetic.main.activity_about.*
 import org.jetbrains.anko.email
-import org.jetbrains.anko.makeCall
 
 class AboutActivity : AppCompatActivity() {
 
@@ -18,10 +20,35 @@ class AboutActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         callTv.setOnClickListener {
-            makeCall("18002744504")
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:18002744504"))
+            startActivity(intent)
         }
         mailTv.setOnClickListener {
             email(mailTv.text.toString())
         }
+
+        phone1.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:${phone1.text}"))
+            startActivity(intent)
+        }
+
+        phone2.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:${phone2.text}"))
+            startActivity(intent)
+        }
+
+        email1.setOnClickListener {
+            email(mailTv.text.toString())
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
