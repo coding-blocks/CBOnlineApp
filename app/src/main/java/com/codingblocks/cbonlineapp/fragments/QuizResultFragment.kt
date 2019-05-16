f6566b4e457e6a1a1b558f7ec3ea804d07550ecc
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2019-05-17 04:52:40 +0530

    fixed card alignment in quiz result

--- app/src/main/java/com/codingblocks/cbonlineapp/fragments/QuizResultFragment.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/fragments/QuizResultFragment.kt
@@ -1,16 +1,13 @@
 package com.codingblocks.cbonlineapp.fragments
 
 import android.content.res.ColorStateList
-import android.graphics.Color
-import android.graphics.Color.*
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffColorFilter
+import android.graphics.Color.BLACK
+import android.graphics.Color.GREEN
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import com.codingblocks.cbonlineapp.R
 import com.codingblocks.cbonlineapp.extensions.retrofitCallback
