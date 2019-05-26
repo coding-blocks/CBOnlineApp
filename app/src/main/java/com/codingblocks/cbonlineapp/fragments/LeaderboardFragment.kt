08d1d4ff1ffd6da67d4538dee169f9fd63aba597
Sanjeev Sharma <thedevelopersanjeev@gmail.com>
2019-05-26 16:06:46 +0530

    reading runid from database

--- app/src/main/java/com/codingblocks/cbonlineapp/fragments/LeaderboardFragment.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/fragments/LeaderboardFragment.kt
@@ -8,20 +8,39 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.codingblocks.cbonlineapp.R
 import com.codingblocks.cbonlineapp.adapters.LeaderboardListAdapter
+import com.codingblocks.cbonlineapp.database.AppDatabase
 import com.codingblocks.cbonlineapp.extensions.retrofitCallback
+import com.codingblocks.cbonlineapp.util.COURSE_ID
+import com.codingblocks.cbonlineapp.util.RUN_ATTEMPT_ID
 import com.codingblocks.cbonlineapp.util.RUN_ID
 import com.codingblocks.onlineapi.Clients
 import com.codingblocks.onlineapi.models.Leaderboard
 import kotlinx.android.synthetic.main.fragment_leaderboard.*
+import org.jetbrains.anko.doAsync
+import kotlin.concurrent.thread
 
 class LeaderboardFragment : Fragment() {
 
-    lateinit var runId: String
+    lateinit var attemptId: String
+    lateinit var runId : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            runId = it.getString(RUN_ID)
+            attemptId = it.getString(RUN_ATTEMPT_ID)
+        }
+
+        getRunId()
+    }
+
+    private fun getRunId() {
+        val database = AppDatabase.getInstance(context!!)
+        val courseDao = database.courseRunDao()
+        thread {
+            doAsync {
+                val courseRun = courseDao.getRunById(attemptId)
+                runId = courseRun.crUid
+            }
         }
     }
 
@@ -47,7 +66,7 @@ class LeaderboardFragment : Fragment() {
         fun newInstance(param1: String) =
             LeaderboardFragment().apply {
                 arguments = Bundle().apply {
-                    putString(RUN_ID, param1)
+                    putString(RUN_ATTEMPT_ID, param1)
                 }
             }
     }
