6b3f4b64ce8b300d49b72862c131f244fc3970d3
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-05-30 17:18:04 +0530

    some minor fixes

--- app/src/main/java/com/codingblocks/cbonlineapp/adapters/MyCoursesViewHolder.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/adapters/MyCoursesViewHolder.kt
@@ -8,14 +8,27 @@ import com.codingblocks.cbonlineapp.R
 import com.codingblocks.cbonlineapp.activities.MyCourseActivity
 import com.codingblocks.cbonlineapp.database.AppDatabase
 import com.codingblocks.cbonlineapp.database.CourseDao
-import com.codingblocks.cbonlineapp.database.models.CourseRun
 import com.codingblocks.cbonlineapp.database.CourseWithInstructorDao
+import com.codingblocks.cbonlineapp.database.models.CourseRun
 import com.codingblocks.cbonlineapp.extensions.loadSvg
+import com.codingblocks.cbonlineapp.util.COURSE_ID
 import com.codingblocks.cbonlineapp.util.COURSE_NAME
 import com.codingblocks.cbonlineapp.util.RUN_ATTEMPT_ID
 import com.codingblocks.cbonlineapp.util.RUN_ID
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.my_course_card_horizontal.view.*
+import kotlinx.android.synthetic.main.my_course_card_horizontal.view.courseBtn1
+import kotlinx.android.synthetic.main.my_course_card_horizontal.view.courseCoverImgView
+import kotlinx.android.synthetic.main.my_course_card_horizontal.view.courseDescription
+import kotlinx.android.synthetic.main.my_course_card_horizontal.view.courseInstrucImgView1
+import kotlinx.android.synthetic.main.my_course_card_horizontal.view.courseInstrucImgView2
+import kotlinx.android.synthetic.main.my_course_card_horizontal.view.courseInstructors
+import kotlinx.android.synthetic.main.my_course_card_horizontal.view.courseLogo
+import kotlinx.android.synthetic.main.my_course_card_horizontal.view.courseProgress
+import kotlinx.android.synthetic.main.my_course_card_horizontal.view.courseRatingBar
+import kotlinx.android.synthetic.main.my_course_card_horizontal.view.courseRatingTv
+import kotlinx.android.synthetic.main.my_course_card_horizontal.view.courseRunDescription
+import kotlinx.android.synthetic.main.my_course_card_horizontal.view.courseTitle
+import kotlinx.android.synthetic.main.my_course_card_horizontal.view.trialTv
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.intentFor
@@ -36,66 +49,70 @@ class MyCoursesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), A
             doAsync {
                 val data = courseDao.getCourse(courseRun.crCourseId)
                 uiThread {
-                data.run {
-                    itemView.courseTitle.text = title
-                    itemView.courseDescription.text = subtitle
-                    itemView.courseRatingTv.text = rating.toString()
-                    itemView.courseRatingBar.rating = rating
-                    itemView.courseRunDescription.text = courseRun.crDescription
-                    itemView.courseProgress.progress = courseRun.progress.toInt()
-                    itemView.courseCoverImgView.loadSvg(coverImage)
-                    itemView.courseLogo.loadSvg(logo)
-                    if (!courseRun.premium) {
-                        itemView.trialTv.visibility = View.VISIBLE
-                    } else {
-                        itemView.trialTv.visibility = View.GONE
-                    }
-                    if (courseRun.crEnd.toLong() * 1000 > System.currentTimeMillis()) {
-                        if (courseRun.progress == 0.0) {
-                            itemView.courseBtn1.text = "Start"
+                    data.run {
+                        itemView.courseTitle.text = title
+                        itemView.courseDescription.text = subtitle
+                        itemView.courseRatingTv.text = rating.toString()
+                        itemView.courseRatingBar.rating = rating
+                        itemView.courseRunDescription.text = courseRun.crDescription
+                        itemView.courseProgress.progress = courseRun.progress.toInt()
+                        itemView.courseCoverImgView.loadSvg(coverImage)
+                        itemView.courseLogo.loadSvg(logo)
+                        if (!courseRun.premium) {
+                            itemView.trialTv.visibility = View.VISIBLE
                         } else {
-                            itemView.courseBtn1.text = "Resume"
+                            itemView.trialTv.visibility = View.GONE
                         }
-                        itemView.courseBtn1.isEnabled = true
-                        itemView.courseBtn1.background = context.getDrawable(R.drawable.button_background)
-                        itemView.courseBtn1.setOnClickListener {
-                            it.context.startActivity(it.context.intentFor<MyCourseActivity>(
-                                RUN_ATTEMPT_ID to courseRun.crAttemptId, COURSE_NAME to title, RUN_ID to courseRun.crUid).singleTop())
+                        if (courseRun.crEnd.toLong() * 1000 > System.currentTimeMillis()) {
+                            if (courseRun.progress == 0.0) {
+                                itemView.courseBtn1.text = "Start"
+                            } else {
+                                itemView.courseBtn1.text = "Resume"
+                            }
+                            itemView.courseBtn1.isEnabled = true
+                            itemView.courseBtn1.background =
+                                context.getDrawable(R.drawable.button_background)
+                            itemView.courseBtn1.setOnClickListener {
+                                it.context.startActivity(
+                                    it.context.intentFor<MyCourseActivity>(
+                                        COURSE_ID to id,
+                                        RUN_ATTEMPT_ID to courseRun.crAttemptId,
+                                        COURSE_NAME to title,
+                                        RUN_ID to courseRun.crUid
+                                    ).singleTop()
+                                )
+                            }
+                        } else {
+                            itemView.courseBtn1.text = "Expired"
+                            itemView.courseBtn1.isEnabled = false
+                            itemView.courseBtn1.background =
+                                context.getDrawable(R.drawable.button_disable)
                         }
-//                        itemView.setOnClickListener {
-//                            it.context.startActivity(it.context.intentFor<MyCourseActivity>("course_id" to id, "attempt_id" to courseRun.crAttemptId, "courseName" to title).singleTop())
-//
-//                        }
-                    } else {
-                        itemView.courseBtn1.text = "Expired"
-                        itemView.courseBtn1.isEnabled = false
-                        itemView.courseBtn1.background = context.getDrawable(R.drawable.button_disable)
                     }
-                }
 
-                // bind Instructors
-                val instructorsLiveData = instructorDao.getInstructorWithCourseId(data.id)
+                    // bind Instructors
+                    val instructorsLiveData = instructorDao.getInstructorWithCourseId(data.id)
                     instructorsLiveData.observe({ (context as LifecycleOwner).lifecycle }, {
                         val instructorsList = it
 
                         var instructors = ""
 
-                            for (i in 0 until instructorsList.size) {
-                                if (i == 0) {
-                                    Picasso.with(context).load(instructorsList[i].photo)
-                                            .fit().into(itemView.courseInstrucImgView1)
-                                    instructors += instructorsList[i].name
-                                } else if (i == 1) {
-                                    itemView.courseInstrucImgView2.visibility = View.VISIBLE
-                                    Picasso.with(context).load(instructorsList[i].photo)
-                                            .fit().into(itemView.courseInstrucImgView2)
-                                    instructors += ", ${instructorsList[i].name}"
-                                } else if (i >= 2) {
-                                    instructors += "+ " + (instructorsList.size - 2) + " more"
-                                    break
-                                }
+                        for (i in 0 until instructorsList.size) {
+                            if (i == 0) {
+                                Picasso.with(context).load(instructorsList[i].photo)
+                                    .fit().into(itemView.courseInstrucImgView1)
+                                instructors += instructorsList[i].name
+                            } else if (i == 1) {
+                                itemView.courseInstrucImgView2.visibility = View.VISIBLE
+                                Picasso.with(context).load(instructorsList[i].photo)
+                                    .fit().into(itemView.courseInstrucImgView2)
+                                instructors += ", ${instructorsList[i].name}"
+                            } else if (i >= 2) {
+                                instructors += "+ " + (instructorsList.size - 2) + " more"
+                                break
                             }
-                            itemView.courseInstructors.text = instructors
+                        }
+                        itemView.courseInstructors.text = instructors
                     })
                 }
             }
