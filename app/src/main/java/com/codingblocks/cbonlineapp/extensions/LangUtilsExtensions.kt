1dbcaf24e3d029683e4ebc819040cee1897a03ca
Arnav Gupta <arnav@codingblocks.com>
2019-06-02 19:28:02 +0530

    router
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ app/src/main/java/com/codingblocks/cbonlineapp/extensions/LangUtilsExtensions.kt
@@ -0,0 +1,6 @@
+package com.codingblocks.cbonlineapp.extensions
+
+
+infix fun Boolean.then(block: () -> Unit) = if (this) block() else null
+
+infix fun Boolean.otherwise(block: () -> Unit) = if (this) null else block()
