d655712e6745638e79e3b6b90bdcd6c6f9f662d5
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-06-06 17:23:12 +0530

    add files

--- app/src/main/java/com/codingblocks/cbonlineapp/widgets/GradientTextView.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/widgets/GradientTextView.kt
@@ -12,16 +12,15 @@ import com.codingblocks.cbonlineapp.R
 
 class GradientTextView : TextView {
 
-    constructor(context: Context) : super(context) {}
+    constructor(context: Context) : super(context)
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
-    ) {
-    }
+    )
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
