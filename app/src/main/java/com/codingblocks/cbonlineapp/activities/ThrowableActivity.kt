fc8fcfd5ba8c603688708300dbef4c48035e4daa
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-06-06 17:19:07 +0530

    layout ready

---
+++ app/src/main/java/com/codingblocks/cbonlineapp/activities/ThrowableActivity.kt
@@ -0,0 +1,12 @@
+package com.codingblocks.cbonlineapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThrowableActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_throwable)
+    }
+}
