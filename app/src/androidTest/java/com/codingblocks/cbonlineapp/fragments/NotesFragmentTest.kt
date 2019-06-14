6541038e003640851be0805893b8a8847c05bab7
raystatic <rahul9650ray@gmail.com>
2019-06-15 01:38:18 +0530

    Written instrumentation tests

---
+++ app/src/androidTest/java/com/codingblocks/cbonlineapp/fragments/NotesFragmentTest.kt
@@ -0,0 +1,37 @@
+package com.codingblocks.cbonlineapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.runner.AndroidJUnit4
+import com.codingblocks.cbonlineapp.R
+import com.codingblocks.cbonlineapp.viewactions.CustomViewAction
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class NotesFragmentTest {
+
+    @Before
+    fun setUp(){
+        FragmentScenario.launchInContainer(VideoNotesFragment::class.java, Bundle().apply {
+            putString(com.codingblocks.cbonlineapp.util.RUN_ATTEMPT_ID,"22685")
+        }, R.style.Theme_CB_Course,null)
+        Thread.sleep(10000)
+    }
+
+
+    @Test
+    fun testNotesRecyclerView(){
+        onView(withId(R.id.notesRv)).check(matches(isDisplayed())).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,CustomViewAction().clickChildViewWithId("Edit")))
+    }
+
+
+
+}
