c31d0a5e6dc74bd7ecc2ab4d2c87d66d5d214b0b
Pulkit <aggarwalpulkit596@gmail.com>
2019-12-18 13:57:26 +0530

    toolbar design completed[skip CI]
    Signed-off-by: Pulkit <aggarwalpulkit596@gmail.com>

--- app/src/main/java/com/codingblocks/cbonlineapp/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/dashboard/DashboardActivity.kt
@@ -1,14 +1,38 @@
 package com.codingblocks.cbonlineapp.dashboard
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.codingblocks.cbonlineapp.R
+import com.codingblocks.cbonlineapp.notifications.NotificationsActivity
 import com.codingblocks.cbonlineapp.util.extensions.replaceFragmentSafely
 import com.codingblocks.fabnavigation.FabNavigation
 import com.codingblocks.fabnavigation.FabNavigationAdapter
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_dashboard.*
 import kotlinx.android.synthetic.main.app_bar_dashboard.*
+import org.jetbrains.anko.intentFor
 
-class DashboardActivity : AppCompatActivity() {
+class DashboardActivity : AppCompatActivity(), DrawerLayout.DrawerListener, NavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return true
+    }
+
+    override fun onDrawerStateChanged(newState: Int) {
+    }
+
+    override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+    }
+
+    override fun onDrawerClosed(drawerView: View) {
+    }
+
+    override fun onDrawerOpened(drawerView: View) {
+    }
 
     //    private val viewModel by viewModel<DashboardViewModel>()
     private val navigationAdapter: FabNavigationAdapter by lazy {
@@ -22,6 +46,20 @@ class DashboardActivity : AppCompatActivity() {
     }
 
     private fun initializeUI() {
+        setSupportActionBar(dashboardToolbar)
+        title = "Dashboard"
+        val toggle = ActionBarDrawerToggle(
+            this,
+            dashboardDrawer,
+            dashboardToolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        dashboardDrawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        dashboardDrawer.addDrawerListener(this)
+        dashboardNavigation.setNavigationItemSelectedListener(this)
         navigationAdapter.setupWithBottomNavigation(dashboardBottomNav)
         dashboardBottomNav.apply {
             defaultBackgroundColor = getColor(R.color.dark)
@@ -44,6 +82,23 @@ class DashboardActivity : AppCompatActivity() {
             containerViewId = R.id.dashboardPager,
             allowStateLoss = true
         )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.dashboard, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        R.id.dashboard_notification -> {
+            startActivity(intentFor<NotificationsActivity>())
+            true
+        }
+        R.id.dashboard_cart -> {
+            //Todo - Implement this
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
 
