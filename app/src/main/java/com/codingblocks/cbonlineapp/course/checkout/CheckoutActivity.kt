fa250a66f3fb4aa65900433274bde4dd179670f4
tdevm <cr14b17@gmail.com>
2020-01-17 22:11:54 +0530

    setup razorpay

--- app/src/main/java/com/codingblocks/cbonlineapp/course/checkout/CheckoutActivity.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/course/checkout/CheckoutActivity.kt
@@ -5,14 +5,38 @@ import androidx.appcompat.app.AppCompatActivity
 import com.codingblocks.cbonlineapp.R
 import com.codingblocks.cbonlineapp.util.extensions.replaceFragmentSafely
 import com.codingblocks.cbonlineapp.util.extensions.setToolbar
+import com.razorpay.Checkout
+import com.razorpay.PaymentResultListener
 import kotlinx.android.synthetic.main.activity_checkout.*
 
-class CheckoutActivity : AppCompatActivity() {
+class CheckoutActivity : AppCompatActivity(), PaymentResultListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkout)
         setToolbar(checkoutToolbar)
+        Checkout.preload(applicationContext)
+
         replaceFragmentSafely(CheckoutOrderDetailsFragment(), containerViewId = R.id.checkoutContainer)
+    }
+
+
+    override fun onPaymentError(p0: Int, p1: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        /** Make Network call to capture payment.
+         *  Body : {
+         *
+         *       razorpay_payment_id: payment_ksdnvsdlv,
+         *       razorpay_order_id: order_ijkdbsn,
+         *       txnId: 192721,
+         *       amount: 2394723 (paise)
+         *  }
+         */
+
+    }
+
+    override fun onPaymentSuccess(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
