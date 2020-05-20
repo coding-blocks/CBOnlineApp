d482abd191c1ad9790dee568ae29eeac024b8c64
shivamgohri <shivamgohri93@gmail.com>
2020-05-20 05:39:52 +0530

    fixed: note timestamp

--- app/src/main/java/com/codingblocks/cbonlineapp/mycourse/player/VideoPlayerActivity.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/mycourse/player/VideoPlayerActivity.kt
@@ -8,7 +8,6 @@ import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
-import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.view.WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE
