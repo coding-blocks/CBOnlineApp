048834325990c116db7f93206b32f699b297461d
Pratham <Prathamkhurana43@gmail.com>
2020-08-05 19:17:35 +0530

    Optimized imports

--- app/src/main/java/com/codingblocks/cbonlineapp/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/dashboard/DashboardActivity.kt
@@ -12,7 +12,6 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.core.view.isVisible
